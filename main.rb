2ba74440d77c2fdbb3ecfd4031e202f00dee25f1
PhillipKri <irrationalman26@gmail.com>
2024-09-29 22:42:59 +0200

    player class and game class mostly finished.

---
+++ main.rb
@@ -0,0 +1,5 @@
+require_relative '/home/phillip/repos/ruby-tic-tac-toe/lib/person'
+require_relative '/home/phillip/repos/ruby-tic-tac-toe/lib/game'
+include TicTacToe
+
+Game.new(Player, Player).play
